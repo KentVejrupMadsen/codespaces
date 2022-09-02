f17168747e4a9b4608419f1878d3719175aa2225
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-02 19:32:36 +0200

    added pull script

---
+++ spaces/pull.sh
@@ -0,0 +1,29 @@
+#!/env/bin/bash
+
+cd bash
+git pull
+cd ..
+
+cd csharp
+git pull
+cd ..
+
+cd java
+git pull
+cd ..
+
+cd javascript
+git pull
+cd ..
+
+cd php
+git pull
+cd ..
+
+cd python
+git pull
+cd ..
+
+cd rust
+git pull
+cd ..
