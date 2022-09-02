cf0d4bf6804267cc9ea53f8ada0ce7e0e984f3eb
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-02 19:24:44 +0200

    a

---
+++ spaces/snapshot.sh
@@ -0,0 +1,69 @@
+#!/env/bin/bash
+# snapshot of bash
+cd bash
+
+git add .
+git commit -m "snapshot"
+git push
+
+cd ..
+
+
+# snapshot of c#
+cd csharp
+
+git add .
+git commit -m "snapshot"
+git push
+
+cd ..
+
+
+# snapshot of java
+cd java
+
+git add .
+git commit -m "snapshot"
+git push
+
+cd ..
+
+
+# snapshot javascript
+cd javascript
+
+git add .
+git commit -m "snapshot"
+git push
+
+cd ..
+
+
+# snapshot of php
+cd php
+
+git add .
+git commit -m "snapshot"
+git push
+
+cd ..
+
+
+# snapshot of Python
+cd python
+
+git add .
+git commit -m "snapshot"
+git push
+
+cd ..
+
+
+# Snapshop of rust
+cd rust
+
+git add .
+git commit -m "snapshot"
+git push
+
+cd ..
